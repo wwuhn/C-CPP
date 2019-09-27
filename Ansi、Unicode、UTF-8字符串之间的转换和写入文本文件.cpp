42236a71d57e3b8803a775edd72ac4a5f64d4fac
wwu <42328371+wwuhn@users.noreply.github.com>
2019-09-27 19:58:14 +0800

    Update Ansi、Unicode、UTF-8字符串之间的转换和写入文本文件.cpp

--- Ansi、Unicode、UTF-8字符串之间的转换和写入文本文件.cpp
+++ Ansi、Unicode、UTF-8字符串之间的转换和写入文本文件.cpp
@@ -1,21 +1,38 @@
-Ansi字符串我们最熟悉，英文占一个字节，汉字2个字节，以一个\0结尾，常用于txt文本文件
-Unicode字符串，每个字符(汉字、英文字母)都占2个字节，以2个连续的\0结尾，NT操作系统内核用的是这种字符串，常被定义为typedef unsigned short wchar_t;所以我们有时常会见到什么char*无法转换为unsigned short*之类的错误，其实就是unicode
-UTF8是 Unicode一种压缩形式，英文A在unicode中表示为0x0041，老外觉得这种存储方式太浪费，因为浪费了50%的空间，于是就把英文压缩成1 个字节，成了utf8编码，但是汉字在utf8中占3个字节，显然用做中文不如ansi合算，这就是中国的网页用作ansi编码而老外的网页常用utf8 的原因。
-UTF8在还游戏里运用的很广泛，比如WOW的lua脚本等
+/*
+Ansi字符串我们最熟悉，英文占一个字节，汉字2个字节，以一个\0结尾，常用于txt文本文件。
+Unicode字符串，每个字符(汉字、英文字母)都占2个字节，以2个连续的\0结尾，NT操作系统内核用的是这种字符串，常被定义为typedef unsigned short wchar_t;
+所以我们有时常会见到什么char*无法转换为unsigned short*之类的错误，其实就是unicode。
+UTF8是 Unicode一种压缩形式，英文A在unicode中表示为0x0041，老外觉得这种存储方式太浪费，因为浪费了50%的空间，于是就把英文压缩成1 个字节，
+成了utf8编码，但是汉字在utf8中占3个字节，显然用做中文不如ansi合算，这就是中国的网页用作ansi编码而老外的网页常用utf8 的原因。
+UTF8在还游戏里运用的很广泛，比如WOW的lua脚本等。
 
 下面来说一下转换，主要用代码来说明吧
 
-写文件我用了CFile类，其实用FILE*之类的也是一样，写文件和字符串什么类别没有关系，硬件只关心数据和长度
+写文件我用了CFile类，其实用FILE*之类的也是一样，写文件和字符串什么类别没有关系，硬件只关心数据和长度。
 
 Ansi转Unicode
 
 介绍2种方法
+UTF-8-bom
+标识位：0XBFBBEF（3个字节）
+换 行符：0x0A0D
+汉字采用三个字节编码
+UTF-8
+标识位：无
+换行符：0X0A0D
+汉字采用三个字节编码
+Unicode Big Endian(UTF-16/UCS-2大端字节序
+Unicode little Endian(UTF-16/UCS-2小端字节序
+标识位：0xFFFE
+换行符标识位：0x0A000D00
+汉字编码：采用2个字节
+*/
 void CConvertDlg::OnBnClickedButtonAnsiToUnicode()
 {
     // ansi to unicode
     char* szAnsi = "abcd1234你我他";
     //预转换，得到所需空间的大小
-    int wcsLen = ::MultiByteToWideChar(CP_ACP, NULL, szAnsi, strlen(szAnsi), NULL, 0);
+    int wcsLen = ::MultiByteToWideChar(CP_ACP, NULL, szAnsi, strlen(szAnsi), NULL, 0); //#include <windows.h>
     //分配空间要给'\0'留个空间，MultiByteToWideChar不会给'\0'空间
     wchar_t* wszString = new wchar_t[wcsLen + 1];
     //转换
@@ -52,9 +69,9 @@ void CConvertDlg::OnBnClickedButtonAnsiToUnicode()
     ::MessageBoxW(GetSafeHwnd(), wcsStr, wcsStr, MB_OK);
 
 }
-复制代码
-Unicode转Ansi
-也是2种方法
+
+//Unicode转Ansi
+//也是2种方法
 void CConvertDlg::OnBnClickedButtonUnicodeToAnsi()
 {
     // unicode to ansi
@@ -94,8 +111,8 @@ void CConvertDlg::OnBnClickedButtonUnicodeToAnsi()
     sprintf(szStr, "%S", wszString);
     ::MessageBoxA(GetSafeHwnd(), szStr, szStr, MB_OK);
 }
-复制代码
-Unicode转UTF8
+
+//Unicode转UTF8
 void CConvertDlg::OnBnClickedButtonUnicodeToU8()
 {
     // unicode to UTF8
@@ -128,8 +145,8 @@ void CConvertDlg::OnBnClickedButtonUnicodeToU8()
     szU8 =NULL;
 
 }
-复制代码
-UTF8转UNICODE
+
+//UTF8转UNICODE
 void CConvertDlg::OnBnClickedButtonU8ToUnicode()
 {
     //UTF8 to Unicode
@@ -148,5 +165,5 @@ void CConvertDlg::OnBnClickedButtonU8ToUnicode()
 
     //写文本同ansi to unicode
 }
-复制代码
-Ansi转换utf8和utf8转换Ansi就是上面2个的结合，把unicode作为中间量，进行2次转换即可
+
+//Ansi转换utf8和utf8转换Ansi就是上面2个的结合，把unicode作为中间量，进行2次转换即可
